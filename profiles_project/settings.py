a04df762a236c9c2973f39b2186618697d0f73ff
Girija <raogirija.04@gmail.com>
2020-06-17 14:17:18 +0530

    deploy

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tp)3=f)cd^ik)*e)c$0ru%ck0&qfyi6l!t15j5@!bebr6)&5%('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =(bool(int(os.environ.get('DEBUG',1))))
 
 ALLOWED_HOSTS = []
 
@@ -122,3 +122,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 AUTH_USER_MODEL='profiles_api.UserProfile'
+
+STATIC_ROOT='static/'
